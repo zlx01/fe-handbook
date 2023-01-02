de89f0c5a64e4f6b7f3cf5d8e6c3e0edb6586897
zlx01 <zlx01@users.noreply.github.com>
2023-01-02 07:37:49 +0000

    deploy: 5aa84ab09b8b8340614fdab5c7f2fc88e30ab5ba

--- assets/build-tools_package-manager_pnpm_index.md.d4359dea.js
+++
@@ -1,6 +0,0 @@
-import{_ as a,c as n,o as s,d as e}from"./app.1b152e31.js";const f=JSON.parse('{"title":"pnpm","description":"","frontmatter":{},"headers":[],"relativePath":"build-tools/package-manager/pnpm/index.md","lastUpdated":1670337782000}'),p={name:"build-tools/package-manager/pnpm/index.md"},l=e(`<h1 id="pnpm" tabindex="-1"><a href="https://pnpm.io/" target="_blank" rel="noreferrer">pnpm</a> <a class="header-anchor" href="#pnpm" aria-hidden="true">#</a></h1><div class="language-bash"><button class="copy"></button><span class="lang">bash</span><pre><code><span class="line"><span style="color:#676E95;"># \u4E0D\u66F4\u65B0 pnpm-lock.yaml</span></span>
-<span class="line"><span style="color:#A6ACCD;">pnpm i --frozen-lockfile</span></span>
-<span class="line"></span>
-<span class="line"><span style="color:#676E95;"># \u53EA\u66F4\u65B0 pnpm-lock.yaml</span></span>
-<span class="line"><span style="color:#A6ACCD;">pnpm i --lockfile-only</span></span>
-<span class="line"></span></code></pre></div>`,2),t=[l];function o(c,r,i,d,_,m){return s(),n("div",null,t)}const u=a(p,[["render",o]]);export{f as __pageData,u as default};
