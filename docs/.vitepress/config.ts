7054874aff1e38bd5e21961a502a56a6da24be05
zlx01 <761451281@qq.com>
2026-05-25 22:46:41 +0800

    feat: integrate markdown-it-container and vitepress-plugin-sandpack
    - Added markdown-it-container for custom markdown containers in the documentation.
    - Configured VitePress to use the sandbox container for rendering.
    - Updated package.json to include markdown-it-container and vitepress-plugin-sandpack as dependencies.
    - Updated yarn.lock to reflect the new dependencies and their versions.
    - Created a new theme index file to register the Sandbox component from vitepress-plugin-sandpack.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,6 @@
 import {DefaultTheme, defineConfig} from 'vitepress'
+import container from 'markdown-it-container'
+import {renderSandbox} from 'vitepress-plugin-sandpack'
 
 const base = process.env.DEPLOY_BASE
 
@@ -22,6 +24,13 @@ export default defineConfig({
     // image: {
     //   lazyLoading: true,
     // }
+    config(md) {
+      md.use(container, 'sandbox', {
+        render(tokens, idx) {
+          return renderSandbox(tokens, idx, 'sandbox')
+        },
+      })
+    },
   },
   ignoreDeadLinks: [
     // ignore all localhost links
